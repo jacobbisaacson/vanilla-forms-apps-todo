cc02f45ad7f9039af8fc9c89026f66f75d7a4bdd
Reuben Ayres <ra@sent.com>
2020-03-09 11:20:27 -0500

    put input and button in a form

--- js/app.js
+++ js/app.js
@@ -12,3 +12,20 @@ addItemButton.addEventListener('click', () => {
   console.log("the add item button was clicked");
 })
 
+
+// the above pattern works great for clicks but forms are designed
+// to communicate information to servers
+// instead of listening for 'click' on the button
+// we should listen for 'submit' on the form
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+// The default behavior of form submission is to try to send 
+// form info to a server
+// so instead of listening for `click` on button, 
+// we want to listen for 'submit' on the form
+// this is a totally separate event
+
+
+
+
+
+
