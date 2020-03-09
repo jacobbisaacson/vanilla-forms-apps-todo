aa690cf4281e6c99fd560db2ac02b7d482180e4c
Reuben Ayres <ra@sent.com>
2020-03-09 11:07:37 -0500

    add item button has a listener that console.logs

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@ console.log("form inputs, to-do list, patterns for building apps");
 
 
 // add a listener to make a console log happen when the button is clicked
+// 1. grab button with id #add-item
+const addItemButton = document.querySelector('#add-item')
+
+// 2. add listener to the button that listens for click and provide that
+// listener with an arrow function to run 
+addItemButton.addEventListener('click', () => {
+  console.log("the add item button was clicked");
+})
+
